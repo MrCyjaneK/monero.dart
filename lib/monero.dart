200ad221441c6481439c966288b6e8834769ed21
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-05-15 12:12:52 +0200

    WIP: ios fix

--- lib/monero.dart
+++ lib/monero.dart
@@ -83,7 +83,7 @@ MoneroC? lib;
 String libPath = (() {
   if (Platform.isWindows) return 'monero_libwallet2_api_c.dll';
   if (Platform.isMacOS) return 'monero_libwallet2_api_c.dylib';
-  if (Platform.isIOS) return 'monero_libwallet2_api_c.dylib';
+  if (Platform.isIOS) return 'MoneroWallet.framework/MoneroWallet';
   if (Platform.isAndroid) return 'libmonero_libwallet2_api_c.so';
   return 'monero_libwallet2_api_c.so';
 })();
